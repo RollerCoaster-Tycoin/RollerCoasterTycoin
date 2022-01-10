23b08a3d4fe7af35ebe7bb50f373836cf95b0ff7
RJK1808 <40627146+RJK1808@users.noreply.github.com>
2022-01-10 00:05:28 +0000

    Update PlayerWorld.sol

--- contracts/PlayerWorld.sol
+++ contracts/PlayerWorld.sol
@@ -1,8 +1,17 @@
-// SPDX-License-Identifier: MIT
 pragma solidity >=0.4.22 <0.9.0;
 
+// Dependencies
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
+// Interface setup
+interface ERCItem {
+  function mint(address account, uint256 amount) external;
+  function burn(address account, uint256 amount) external;
+  function balance(address account) external returns (uint256);
+
+  function stake(address account, uint256 amount) external;
+  function getStakedAmount(address account) external returns (uint256);
+}
 
 contract PlayerWorld {
   constructor() public {
