6c731bc5d5c2acca24555d768c6aa45282b6ad47
RJK1808 <40627146+RJK1808@users.noreply.github.com>
2022-01-10 00:41:33 +0000

    Added Contract Administration
    Added Administered.sol, handles user roles.
    Started ContractPausable.sol contract.

---
+++ contracts/Administered.sol
@@ -0,0 +1,45 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+// Dependencies
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+/**
+    @title Administration Contract
+    @author RossK
+    @notice Implements roles.
+ */
+contract Administered is AccessControl {
+    bytes32 public constant USER_ROLE = keccak256("USER");
+
+    // Add root address to admin role.
+    constructor (address root) public {
+        _setupRole(DEFAULT_ADMIN_ROLE, root);
+        _setRoleAdmin(USER_ROLE, DEFAULT_ADMIN_ROLE);
+    }
+
+    // Restricted to members of admin role.
+    modifier OnlyAdmin() {
+        require(CheckAdmin(msg.sender), "This is resricted to administrators");
+        _;
+    }
+
+    // Will return true if the address belongs to the admin role.
+    function CheckAdmin(address account) public virtual view returns (bool) {
+        return hasRole(DEFAULT_ADMIN_ROLE, account);
+    }
+
+    // Add administrator to another address. Restricted to admins.
+    function AddAdmin(address account) public virtual OnlyAdmin {
+        grantRole(DEFAULT_ADMIN_ROLE, account);
+    }
+
+    // Remove administrator from another address. Restricted to admins. (Basically remove admin)
+    function AddUser(address account) public virtual OnlyAdmin {
+        grantRole(USER_ROLE, account);
+    }
+
+    // Remove admin from your address.
+    function SurrenderAdmin() public virtual {
+        renounceRole(DEFAULT_ADMIN_ROLE, msg.sender);
+    }
+}
