562b94bd6566e55d4666cd02bd3af5a46c5ce264
RJK1808 <40627146+RJK1808@users.noreply.github.com>
2022-01-11 00:04:00 +0000

    Updating Enviroment
    Added Truffle to devDependencies.
    Installed Truffle locally.
    Updated ContractPausable.sol metadata.

--- contracts/lifecycle/ContractPausable.sol
+++ contracts/lifecycle/ContractPausable.sol
@@ -3,7 +3,11 @@ pragma solidity >=0.4.22 <0.9.0;
 // Dependencies
 import "../Auth.sol";
 
-
+/**
+    @title Contract Pausing Contract
+    @author RossK
+    @notice Allows administrators to pause.
+ */
 abstract contract ContractPausable is Auth {
 
     bool public Paused = false;
